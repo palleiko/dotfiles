75d92c65fa3450a7dfbd27ca6652cb209e2663d5
palleiko <ian.palleiko@gmail.com>
2017-09-25 21:53:07 -0400

    Making some sweet scripts work

--- .config/conky/batcolor.sh
+++ .config/conky/batcolor.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-PERC=$1
+PERC=`acpi | cut -d, -f2- | cut -d% -f1`
 
-RED=
-YELLOW=
-ORANGE=
-GREEN=
-
-if [ PERC -lt 20 ]; then
-	printf " "
+RED="\#cc6666"
+ORANGE="\#de935f"
+YELLOW="\#f0c674"
+GREEN="\#b5db68"
+echo $PERC
+if [ "$PERC" -gt 0 -a "$PERC" -le 25 ]; then
+	printf "$RED"
+fi
+if [ "$PERC" -gt 25 -a "$PERC" -le 50 ]; then
+	printf "$ORANGE"
+fi
+if [ "$PERC" -gt 50 -a "$PERC" -le 80 ]; then
+	printf "$YELLOW"
+fi
+if [ "$PERC" -gt 80 -a "$PERC" -le 100 ]; then
+	printf "$GREEN"
 fi
--- .config/conky/usagecolor.sh
+++ .config/conky/usagecolor.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-PERC=$1
+# Round
+PERC=$(echo "(`free | grep Mem | awk '{print $3/$2 * 100}'`+0.5)/1" | bc )
 
-RED=
-YELLOW=
-ORANGE=
-GREEN=
+# Trunkate
+#PERC=$(printf "%.0f" `free | grep Mem | awk '{print $3/$2 * 100}'`)
 
-if [ PERC -lt 20 ]; then
-	printf " "
+RED="\#cc6666"
+ORANGE="\#de935f"
+YELLOW="\#f0c674"
+GREEN="\#b5db68"
+
+# Debug echo 
+# echo $PERC
+
+
+if [ "$PERC" -gt 0 -a "$PERC" -le 25 ]; then
+	printf "$GREEN"
+fi
+if [ "$PERC" -gt 25 -a "$PERC" -le 50 ]; then
+	printf "$YELLOW"
+fi
+if [ "$PERC" -gt 50 -a "$PERC" -le 80 ]; then
+	printf "$ORANGE"
+fi
+if [ "$PERC" -gt 80 -a "$PERC" -le 100 ]; then
+	printf "$RED"
 fi
