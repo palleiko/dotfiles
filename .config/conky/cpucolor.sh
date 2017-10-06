d0c50fe32eb8395b39ee0e563b2e79ddcc024048
palleiko <ian.palleiko@gmail.com>
2017-10-06 14:09:51 -0400

    using memcolor.sh & cpucolor.sh for colors in bar

--- .config/conky/cpucolor.sh
+++ .config/conky/cpucolor.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 # Round
-PERC=$(echo "(`free | grep Mem | awk '{print $3/$2 * 100}'`+0.5)/1" | bc )
+PERC=$(echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')])
 
 # Trunkate
 #PERC=$(printf "%.0f" `free | grep Mem | awk '{print $3/$2 * 100}'`)
 
-RED="\#cc6666"
-ORANGE="\#de935f"
-YELLOW="\#f0c674"
-GREEN="\#b5db68"
-
+RED="cc6666"
+ORANGE="de935f"
+YELLOW="f0c674"
+GREY="666666"
 # Debug echo 
-echo $PERC
+# echo $PERC
 
-
-if [ "$PERC" -gt 0 -a "$PERC" -le 25 ]; then
-	printf "$GREEN"
-elif [ "$PERC" -gt 25 -a "$PERC" -le 50 ]; then
-	printf "$YELLOW"
+if [ "$PERC" -gt 0 -a "$PERC" -le 50 ]; then
+	printf "$GREY"
 elif [ "$PERC" -gt 50 -a "$PERC" -le 80 ]; then
 	printf "$ORANGE"
 elif [ "$PERC" -gt 80 -a "$PERC" -le 100 ]; then
