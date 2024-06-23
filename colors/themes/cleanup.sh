90224c4f2bb282c34c26413265ab500100062cab
Ian Palleiko <ian.palleiko@gmail.com>
2024-06-23 00:04:38 -0700

    remove old dotfiles

--- colors/themes/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-files="$(ls base16-i3/colors/)"
-
-for i in $files; do
-   cat base16-i3/colors/$i | sed 's/^.....//g' | sed -n '6,21p' | sed 's/\( #......\)/="\1"/g' | sed 's/ //g' > $i
-
-done
-
